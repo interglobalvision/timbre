fd337b6b05892101e81caf7b8d7acc4032ae924f
cas <casska@gmail.com>
2015-02-18 13:37:08 -0600

    add - Timbres collection and dummy data

---
+++ collections/timbres.js
@@ -0,0 +1,42 @@
+/* ---------------------------------------------------- +/
+## Timbres ##
+/+ ---------------------------------------------------- */
+
+Timbres = new Meteor.Collection('timbres');
+
+if( Meteor.isServer ) {
+  // Create 2dspehere indexes
+  Timbres._ensureIndex({
+    location: "2dsphere",
+  });
+}
+
+// Allow/Deny
+
+Timbres.allow({
+  insert: function(userId, doc){
+    return can.createTimbre(userId);
+  },
+  update:  function(userId, doc, fieldNames, modifier){
+    return can.editTimbre(userId, doc);
+  },
+  remove:  function(userId, doc){
+    return can.removeTimbre(userId, doc);
+  }
+});
+
+// Methods
+
+Meteor.methods({
+  createTimbre: function(timbre){
+    if(can.createTimbre(Meteor.user()))
+      Timbres.insert(timbre);
+  },
+  removeTimbre: function(timbre){
+    if(can.removeTimbre(Meteor.user(), timbre)){
+      Timbres.remove(timbre._id);
+    }else{
+      throw new Meteor.Error(403, 'You do not have the rights to delete this timbre.')
+    }
+  }
+});
--- server/seed.js
+++ server/seed.js
@@ -25,3 +25,54 @@ if (Items.find().count() === 0) {
   });
 
 }
+
+/* Dummy user */
+if( Meteor.users.findOne({ 'emails.address': 'globie@interglobal.vision' }) == undefined ) {
+  Accounts.createUser({
+    email: 'globie@interglobal.vision',
+    password: 'globie'
+  });
+
+  Accounts.createUser({
+    email: 'hugo@interglobal.vision',
+    password: 'hugo'
+  });
+
+  Accounts.createUser({
+    email: 'paco@interglobal.vision',
+    password: 'paco'
+  });
+
+  Accounts.createUser({
+    email: 'luis@interglobal.vision',
+    password: 'luis'
+  });
+}
+
+/* Timbres dataa */
+if (Timbres.find().count() === 0) {
+  var globie = Meteor.users.findOne({ 'emails.address': 'globie@interglobal.vision' }),
+    hugo = Meteor.users.findOne({ 'emails.address': 'hugo@interglobal.vision' }),
+    paco = Meteor.users.findOne({ 'emails.address': 'paco@interglobal.vision' }),
+    luis = Meteor.users.findOne({ 'emails.address': 'luis@interglobal.vision' });
+
+  Timbres.insert({
+    name: "CHAVVOS420",
+    location: {
+      type: "Point",
+      coordinates: [ -99.1490657, 19.4313399 ]
+    },
+    address: "Ayuntamiento 132 - 15, Col. Centro, Cuauhtémoc, DF",
+    users: [ globie._id ]
+  });
+
+  Timbres.insert({
+    name: "Casaa de Hugo",
+    location: {
+      type: "Point",
+      coordinates: [ -99.152845, 19.427591 ]
+    },
+    address: "Ayuntamiento 132 - 15, Col. Centro, Cuauhtémoc, DF",
+    users: [ globie._id ]
+  });
+}
