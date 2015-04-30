171779544b6b96f8885375f32d092da8011aa60d
mikey <michaelrayvon@gmail.com>
2015-04-30 15:57:48 -0500

    add login styling and intro scene

--- client/main.js
+++ client/main.js
@@ -7,3 +7,52 @@ Global client-side code. Loads last.
 /+ ---------------------------------------------------- */
 
 //
+
+Template.login.rendered = function(){
+	var winHeight = $(window).height(),
+	$holder = $('.login-holder'),
+	$street = $('.street'),
+	$building = $('.building'),
+	$cone = $('#cone'),
+	$circle = $('#circle'),
+	$form = $('.login-form'),
+	$music = $('.music'),
+	$note = $('.note');
+
+	resizeScene = function() {
+		$holder.height(winHeight + 'px');
+		$building.height(winHeight - $street.outerHeight());
+	}
+
+	resizeScene();
+	
+	$('.building').each(function() {
+		var windowRowTop = -2;
+		$(this).children('.window-row').each(function() {
+			$(this).css('top',windowRowTop + '%');
+			windowRowTop += 15;
+		});
+	});
+
+	$music.addClass('show-hide');
+
+	var buildingWinHeight = $('.window').height();
+
+	$music.animate({'top': '10%'}, 2000, function() {
+		$cone.animate({'opacity': 0}, 1000, function() {
+			$cone.appendTo('.open-window').css('top', (buildingWinHeight/2));
+			$cone.animate({'opacity': 1}, 1000, function() {
+				var formHeight = $form.css('height', 'auto').height();
+				$form.css('height',0);
+				$form.animate({'height': formHeight, 'opacity': 1}, 1000);
+			})
+		});
+	})
+		
+
+	$(window).on('resize', function() {
+		winHeight = $(window).height();
+		resizeScene();
+	});
+}
+
