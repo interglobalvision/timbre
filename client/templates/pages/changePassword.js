0a792fdf87e332bc4d6b2cb83ff55364d77345e7
mikey <michaelrayvon@gmail.com>
2015-05-27 16:56:43 -0500

    add new passwords validation

--- client/templates/pages/changePassword.js
+++ client/templates/pages/changePassword.js
@@ -2,7 +2,27 @@ Template.login.events = {
 	'click input[type=submit]': function(event){
     event.preventDefault();
 
-    console.log('change password click')
+    var passwords = {
+      oldPassword: $('#old-password').val(),
+      newPassword: $('#new-password').val(),
+      newAgain: $('#new-password-again').val()
+    };
+
+    console.log('change password click');
+
+    if (isValidPassword(passwords.newPassword)) {
+    	if (areSimilarPasswords(passwords.newPassword, passwords.newAgain)) {
+	    	console.log('Your password has been changed.');
+	    	// flash('Your passwords has been changed.');
+	    } else {
+	    	console.log('New passwords are not the same.');
+	    	// flash('New passwords are not the same.');
+	    }
+    } else {
+    	console.log('Your new password should be 6 characters or longer.');
+    	// flash('Your new password should be 6 characters or longer.');
+    }
+    
 
   }
 };
