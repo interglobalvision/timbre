94f84fbbb645b76f5063f40b35f4d03a077bd374
mikey <michaelrayvon@gmail.com>
2015-05-27 16:55:30 -0500

    add signup input validation

--- client/templates/users/signup.js
+++ client/templates/users/signup.js
@@ -8,17 +8,30 @@ Template.signup.events = {
       password: $('#password').val()
     };
 
-    if(!user.username || !user.email || !user.password){
-//       flash('Please fill in all fields');
-    }else{
-      Accounts.createUser(user, function(error){
-        if(error){
-//           flash(error.reason, 'error');
-        }else{
-          Router.go('/');
-//           flash('Thanks for signing up!');
+    if (isNotEmpty(user.username) && isNotEmpty(user.email) && isNotEmpty(user.password)) {
+      if (isEmail(user.email)) {
+        if (isValidPassword(user.password)) {
+          Accounts.createUser(user, function(error){
+            if (error) {
+              console.log(error.reason);
+              // flash(error.reason, 'error');
+            } else {
+              Router.go('/');
+              console.log('Thanks for signing up!');
+              // flash('Thanks for signing up!');
+            }
+          });
+        } else {
+          console.log('Your password should be 6 characters or longer.');
+          // flash('Your password should be 6 characters or longer.')
         }
-      });
+      } else {
+        console.log('Please enter a valid email.');
+        // flash('Please enter a valid email.');
+      }
+    } else { 
+      console.log('Please fill in all fields.');
+      // flash('Please fill in all fields.');
     }
 
   }
