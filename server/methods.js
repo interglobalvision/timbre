febdc6ee7b4b03ffebc409212f1323b642d34ee9
mikey <michaelrayvon@gmail.com>
2015-05-29 13:29:46 -0500

    add checkPassword function

--- server/methods.js
+++ server/methods.js
@@ -30,5 +30,19 @@ Meteor.methods({
       },
       users: [Meteor.user()._id]
     });
-  }
+  },
+
+  checkPassword: function(digest) {
+    check(digest, String);
+
+    if (this.userId) {
+      var user = Meteor.user();
+      var password = {digest: digest, algorithm: 'sha-256'};
+      var result = Accounts._checkPassword(user, password);
+      return result.error == null;
+    } else {
+      return false;
+    }
+  },
+
 });
