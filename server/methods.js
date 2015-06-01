81f3a6d3d98baa9efca5ac297adcf92946118ef5
mikey <michaelrayvon@gmail.com>
2015-06-01 17:41:16 -0500

    add editTimbre method

--- server/methods.js
+++ server/methods.js
@@ -33,6 +33,38 @@ Meteor.methods({
     });
   },
 
+  editTimbre: function (id, data) {
+
+    var name = data[0].value,
+      address = data[1].value,
+      lat = data[2].value,
+      lng = data[3].value;
+
+    // Check argument types
+    check(name, String);
+    check(address, String);
+
+    lat = parseFloat(lat);
+    lng = parseFloat(lng);
+
+/*
+    console.log(name);
+    console.log(address);
+    console.log(lat);
+    console.log(lng);
+    console.log(Meteor.user()._id);
+*/
+
+    return Timbres.insert({
+      name: name,
+      address: address,
+      location: {
+        type: "Point",
+        coordinates: [lng, lat]
+      }
+    });
+  },
+
   checkPassword: function(digest) {
 
     if (this.userId) {
