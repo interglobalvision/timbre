5ad91c7fe24b39f5f860e11855060f8915da6f58
mikey <michaelrayvon@gmail.com>
2015-06-01 17:44:01 -0500

    update editTimbre js.  use init input values for startlocation

--- client/templates/timbre/editTimbre.js
+++ client/templates/timbre/editTimbre.js
@@ -5,27 +5,16 @@ Template.editTimbre.onRendered(function() {
 
       $('#map').height(($(window).height()/2));
 
-      var startLocation = [19.435156, -99.140907];
-
       var locationInputLat = $('#timbre-lat');
       var locationInputLong = $('#timbre-long');
+
+      var startLocation = [locationInputLat.val(), locationInputLong.val()];
+
+      console.log(locationInputLat.val);
 
       L.mapbox.accessToken = 'pk.eyJ1IjoiaW50ZXJnbG9iYWx2aXNpb24iLCJhIjoiVWJ4c3pFayJ9.uetYP9xe-j0wqh4oUN3WxA';
       var map = L.mapbox.map('map', 'interglobalvision.lgbm61l6');
       map.setView(startLocation, 17);
-
-      if (navigator.geolocation){
-        navigator.geolocation.getCurrentPosition(function(position) {
-//           console.log(position);
-          map.setView([position.coords.latitude, position.coords.longitude], 17);
-        }, function(error) {
-          console.log(error);
-        }, {
-          enableHighAccuracy: true,
-          maximumAge : 30000,
-          timeout : 27000
-        });
-      }
 
       var marker = L.marker(startLocation);
       marker.addTo(map);
@@ -47,10 +36,10 @@ Template.editTimbre.onRendered(function() {
 });
 
 Template.editTimbre.events = {
-  'submit #form-create-timbre': function(e) {
+  'submit #form-edit-timbre': function(e) {
     e.preventDefault();
-    var data = $('#form-create-timbre').serializeArray();
-    Meteor.call('createTimbre', data, function (error, result) {
+    var data = $('#form-edit-timbre').serializeArray();
+    Meteor.call('editTimbre', [this.params._id, data], function (error, result) {
       if (error) {
         console.log(error);
       } else {
