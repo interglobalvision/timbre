66b7ef67fed72be04748350b6e4d3e15101dbb18
mikey <michaelrayvon@gmail.com>
2015-06-01 23:54:29 -0500

    add addUsers form submit event

--- client/templates/timbre/addUsers.js
+++ client/templates/timbre/addUsers.js
@@ -13,3 +13,34 @@ Template.addUsers.rendered = function() {
   Meteor.typeahead.inject();
   
 };
+
+Template.addUsers.events = {
+  'submit #form-add-user': function(e) {
+    e.preventDefault();
+    var data = $('#form-add-user').serializeArray(),
+    username = data[0].value;
+
+    if (Meteor.users.findOne({username: username})) {
+
+    	console.log('That username is valid.');
+
+    	data.push(Meteor.users.findOne({username: username})._id);
+    	data.push(this._id);
+      
+			Meteor.call('addUser', data, function (error, result) {
+			  if (error) {
+			    console.log(error);
+			  } else {
+			    console.log('success');
+			    Router.go('/settings');
+			  }
+			});
+
+		} else {
+		console.log('That username is invalid.');
+		// flash('That username is invalid.');
+		}
+
+	}
+
+};
