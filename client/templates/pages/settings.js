e088b5ec0d54eccdb5a0110f33d8fbef6d2c6b5d
mikey <michaelrayvon@gmail.com>
2015-06-01 21:39:57 -0500

    add username length limit check to settings form validation

--- client/templates/pages/settings.js
+++ client/templates/pages/settings.js
@@ -3,26 +3,37 @@ Template.settings.events = {
     e.preventDefault();
     var data = $('#form-user-settings').serializeArray(),
     username = data[0].value,
-    email = data[1].value;
+    email = data[1].value,
+    length = username.length,
+    limit = nameLimit();
 
     console.log(email);
 
     if (isNotEmpty(username) && isNotEmpty(email)) {
-      if (isEmail(email)) {
-    
-        Meteor.call('editUser', data, function (error, result) {
-          if (error) {
-            console.log(error);
-          } else {
-            console.log('success');
-            Router.go('/settings');
-          }
-        });
+
+      if (length <= limit) {
+
+        if (isEmail(email)) {
+      
+          Meteor.call('editUser', data, function (error, result) {
+            if (error) {
+              console.log(error);
+            } else {
+              console.log('success');
+              Router.go('/settings');
+            }
+          });
+
+        } else {
+          console.log('Please enter a valid email.');
+          // flash('Please enter a valid email.');
+        }
 
       } else {
-        console.log('Please enter a valid email.');
-        // flash('Please enter a valid email.');
+        console.log('Your username is over 20 characters. Try something shorter.');
+        // flash('Your username is over 20 characters. Try something shorter.');
       }
+
     } else {
       console.log('Please fill in all fields.');
       // flash('Please fill in all fields.');
