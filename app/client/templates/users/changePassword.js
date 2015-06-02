991e478d37f5075e5717453a97de87fb7785076d
cas <casska@gmail.com>
2015-06-02 16:57:12 -0500

    lint changePassword.js

--- app/client/templates/users/changePassword.js
+++ app/client/templates/users/changePassword.js
@@ -1,5 +1,5 @@
 Template.changePassword.events = {
-	'click input[type=submit]': function(event){
+  'click input[type=submit]': function(event){
     event.preventDefault();
 
     var passwords = {
@@ -16,34 +16,34 @@ Template.changePassword.events = {
 
     Meteor.call('checkPassword', digest, function(err, result) {
       if (result) {
-      	console.log('password checked.');
-		    if (isValidPassword(passwords.newPassword)) {
-		    	console.log('new password is valid');
-		    	if (areSimilarPasswords(passwords.newPassword, passwords.newAgain)) {
-		    		console.log('new passwords are similar');
-		    		Accounts.changePassword(passwords.oldPassword, passwords.newPassword, function(error) {
-			    		if (error) {
-			          console.log('We are sorry but something went wrong.');
-			          // flash('We are sorry but something went wrong.');
-			        } else {
-			          console.log('Your password has been changed.');
-					    	// flash('Your passwords has been changed.');
-					  		Router.go('/settings.html');
-			        }
-			      });
-			    } else {
-			    	console.log('New passwords are not the same.');
-			    	// flash('New passwords are not the same.');
-			    }
-		    } else {
-		    	console.log('Your new password should be 6 characters or longer.');
-		    	// flash('Your new password should be 6 characters or longer.');
-		    }
-		  } else {
-		  	console.log('Your old password is incorrect.');
-	    	// flash('Your old password is incorrect.');
-		  }
-		});
+        console.log('password checked.');
+        if (isValidPassword(passwords.newPassword)) {
+          console.log('new password is valid');
+          if (areSimilarPasswords(passwords.newPassword, passwords.newAgain)) {
+            console.log('new passwords are similar');
+            Accounts.changePassword(passwords.oldPassword, passwords.newPassword, function(error) {
+              if (error) {
+                console.log('We are sorry but something went wrong.');
+                // flash('We are sorry but something went wrong.');
+              } else {
+                console.log('Your password has been changed.');
+                // flash('Your passwords has been changed.');
+                Router.go('/settings.html');
+              }
+            });
+          } else {
+            console.log('New passwords are not the same.');
+            // flash('New passwords are not the same.');
+          }
+        } else {
+          console.log('Your new password should be 6 characters or longer.');
+          // flash('Your new password should be 6 characters or longer.');
+        }
+      } else {
+        console.log('Your old password is incorrect.');
+        // flash('Your old password is incorrect.');
+      }
+    });
 
   }
 };
