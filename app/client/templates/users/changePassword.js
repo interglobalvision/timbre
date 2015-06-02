7f2e221f723a2ad2750a7dc678dc8c385eb9a53f
mikey <michaelrayvon@gmail.com>
2015-06-02 17:43:40 -0500

    clean up changePassword.js and signup.js

--- app/client/templates/users/changePassword.js
+++ app/client/templates/users/changePassword.js
@@ -6,17 +6,10 @@ Template.changePassword.events = {
       oldPassword: $('#old-password').val(),
       newPassword: $('#new-password').val(),
       newAgain: $('#new-password-again').val()
-    };
-
-    console.log('change password click');
-
-    var digest = Package.sha.SHA256(passwords.oldPassword);
-
-    console.log(digest);
+    }, digest = Package.sha.SHA256(passwords.oldPassword);
 
     Meteor.call('checkPassword', digest, function(err, result) {
       if (result) {
-<<<<<<< HEAD
 		    if (isValidPassword(passwords.newPassword)) {
 		    	if (areSimilarPasswords(passwords.newPassword, passwords.newAgain)) {
 		    		Accounts.changePassword(passwords.oldPassword, passwords.newPassword, function(error) {
@@ -42,36 +35,5 @@ Template.changePassword.events = {
 	    	// flash('Your old password is incorrect.');
 		  }
 		});
-=======
-        console.log('password checked.');
-        if (isValidPassword(passwords.newPassword)) {
-          console.log('new password is valid');
-          if (areSimilarPasswords(passwords.newPassword, passwords.newAgain)) {
-            console.log('new passwords are similar');
-            Accounts.changePassword(passwords.oldPassword, passwords.newPassword, function(error) {
-              if (error) {
-                console.log('We are sorry but something went wrong.');
-                // flash('We are sorry but something went wrong.');
-              } else {
-                console.log('Your password has been changed.');
-                // flash('Your passwords has been changed.');
-                Router.go('/settings.html');
-              }
-            });
-          } else {
-            console.log('New passwords are not the same.');
-            // flash('New passwords are not the same.');
-          }
-        } else {
-          console.log('Your new password should be 6 characters or longer.');
-          // flash('Your new password should be 6 characters or longer.');
-        }
-      } else {
-        console.log('Your old password is incorrect.');
-        // flash('Your old password is incorrect.');
-      }
-    });
->>>>>>> 702163dbc1a83c775d898988f66cf6dbbe9685eb
-
   }
 };
--- app/client/templates/users/signup.js
+++ app/client/templates/users/signup.js
@@ -7,28 +7,18 @@ Template.signup.events = {
       email: $('#email').val(),
       password: $('#password').val(),
       repeatPassword: $('#repeat-password').val()
-    };
-
-    var usernameLength = user.username.length,
-      usernameLimit = nameLimit();
-
-    var usernameExists = Meteor.users.findOne({username: user.username});
-
+    },
+      usernameLength = user.username.length,
+      usernameLimit = nameLimit(),
+      usernameExists = Meteor.users.findOne({username: user.username});
 
     if (isNotEmpty(user.username) && isNotEmpty(user.email) && isNotEmpty(user.password)) {
-
       if (usernameLength <= usernameLimit) {
-
         if (!usernameExists) {
-
           if (isEmail(user.email)) {
-
             if (isValidPassword(user.password)) {
-
               if (areSimilarPasswords(user.password, user.repeatPassword)) {
-                
                 Accounts.createUser(user, function(error){
-
                   if (error) {
                     console.log(error.reason);
                     // flash(error.reason, 'error');
@@ -37,38 +27,30 @@ Template.signup.events = {
                     console.log('Thanks for signing up!');
                     // flash('Thanks for signing up!');
                   }
-
                 });
-
               } else {
                 console.log('Oh those passwords dont match.');
                 // flash('Oh those passwords dont match.');
               }
-
             } else {
               console.log('Your password should be 6 characters or longer.');
               // flash('Your password should be 6 characters or longer.')
             }
-
           } else {
             console.log('Please enter a valid email.');
             // flash('Please enter a valid email.');
           }
-
         } else {
           console.log('Ufff that username is taken ~ Try something better.');
           // flash('Ufff that username is taken ~ Try something better.');
         }
-
       } else {
         console.log('Your username is over 20 characters. Try something shorter.');
         // flash('Your username is over 20 characters. Try something shorter.');
       }
-
     } else { 
       console.log('Please fill in all fields.');
       // flash('Please fill in all fields.');
     }
-
   },
 };
