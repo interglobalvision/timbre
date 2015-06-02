d3b495460c5a6f621958cf4d97152899aa10bcb9
mikey <michaelrayvon@gmail.com>
2015-06-02 17:22:23 -0500

    fix signup form validation

--- app/client/templates/users/signup.js
+++ app/client/templates/users/signup.js
@@ -1,41 +1,49 @@
-Template.signup.rendered = function() {
-
-  this.autorun(function () {
-    Meteor.subscribe("allUsernames");
-  });
-
-}
-
 Template.signup.events = {
   'click input[type=submit]': function(event){
     event.preventDefault();
 
     var user = {
       username: $('#username').val(),
-      email: $('#eventmail').val(),
-      password: $('#password').val()
+      email: $('#email').val(),
+      password: $('#password').val(),
+      repeatPassword: $('#repeat-password').val()
     };
 
     var usernameLength = user.username.length,
       usernameLimit = nameLimit();
 
-    var usernameExists = Meteor.users.findOne({username: username});
+    var usernameExists = Meteor.users.findOne({username: user.username});
 
     if (isNotEmpty(user.username) && isNotEmpty(user.email) && isNotEmpty(user.password)) {
+
       if (usernameLength <= usernameLimit) {
+
         if (!usernameExists) {
+
           if (isEmail(user.email)) {
+
             if (isValidPassword(user.password)) {
-              Accounts.createUser(user, function(error){
-                if (error) {
-                  console.log(error.reason);
-                  // flash(error.reason, 'error');
-                } else {
-                  Router.go('/');
-                  console.log('Thanks for signing up!');
-                  // flash('Thanks for signing up!');
-                }
-              });
+
+              if (areSimilarPasswords(user.password, user.repeatPassword)) {
+  /*
+                Accounts.createUser(user, function(error){
+
+                  if (error) {
+                    console.log(error.reason);
+                    // flash(error.reason, 'error');
+                  } else {
+                    Router.go('/');
+                    console.log('Thanks for signing up!');
+                    // flash('Thanks for signing up!');
+                  }
+
+                });
+              */console.log('account created');
+              } else {
+                console.log('Oh those passwords dont match.');
+                // flash('Oh those passwords dont match.');
+              }
+
             } else {
               console.log('Your password should be 6 characters or longer.');
               // flash('Your password should be 6 characters or longer.')
@@ -47,8 +55,8 @@ Template.signup.events = {
           }
 
         } else {
-          console.log('That username already exists! Try another one.');
-          // flash('That username already exists! Try another one.');
+          console.log('Ufff that username is taken ~ Try something better.');
+          // flash('Ufff that username is taken ~ Try something better.');
         }
 
       } else {
