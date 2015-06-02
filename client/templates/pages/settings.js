9e0325bf1e7227ab60209317c5fa91ff647768af
mikey <michaelrayvon@gmail.com>
2015-06-01 19:19:09 -0500

    add form validation to user settings form

--- client/templates/pages/settings.js
+++ client/templates/pages/settings.js
@@ -2,15 +2,27 @@ Template.settings.events = {
   'submit #form-user-settings': function(e) {
     e.preventDefault();
     var data = $('#form-user-settings').serializeArray();
+
+    if (isNotEmpty(data.[0]) && isNotEmpty(data.[1])) {
+      if (isEmail(data.[1])) {
     
-    Meteor.call('editUser', data, function (error, result) {
-      if (error) {
-        console.log(error);
+        Meteor.call('editUser', data, function (error, result) {
+          if (error) {
+            console.log(error);
+          } else {
+            console.log('success');
+            Router.go('/settings');
+          }
+        });
+
       } else {
-        console.log('success');
-        Router.go('/settings');
+        console.log('Please enter a valid email.');
+        // flash('Please enter a valid email.');
       }
-    });
+    } else {
+      console.log('Please fill in all fields.');
+      // flash('Please fill in all fields.');
+    }
     
   }
 };
